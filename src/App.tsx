9df50401cdfcbea96960f3a4d6f8b1b91c781de2
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-27 22:32:09 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from "./App.style";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText> Sistema Financeira </C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* Area de Infformações */}
+
+        {/* Area de Inserção */}
+
+        {/* Tabela de Itens */}
+      </C.Body>
+    </C.Container>
   );
 }
 
